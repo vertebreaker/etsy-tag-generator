87710de09c1b7f60fea3d7687caf29fdf8467db0
vertebreaker <creatiwitty086@gmail.com>
2026-09-24 14:35:55 +0530

    Add sitemap function for site URLs

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://etsy-tag-generator.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+  ];
+}
